1d73090323e34b18c20463127a1b1184bc9783b0
Bettina-Ruth <bettinalearn@gmail.com>
2026-02-25 13:04:37 +0530

    First commit

---
+++ hopeworks.js
@@ -0,0 +1,9 @@
+function showMessage(msg) {
+  alert(msg);
+}
+function goToPage() {
+  window.location.href = "findjobs.html";
+}
+function goToPage2() {
+  window.location.href = "postajob.html";
+}
---
+++ test.js
@@ -0,0 +1,11 @@
+function f1(){
+z=document.getElementById("id2").innerHTML="computadora";
+}
+x=document.getElementById("id1");
+x.style.color="blue";
+y="a";
+if (isNaN(5)){
+	alert("Not a number");
+}
+var a=1;
+var b=2;
